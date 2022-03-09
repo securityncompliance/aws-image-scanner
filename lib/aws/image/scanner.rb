3b76426214419a5efd8665cd1b728520cd2a8a9a
Security and Compliance <main@securityncompliance.com>
2022-03-09 11:24:59 -0500

    Init commit

---
+++ lib/aws/image/scanner.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "scanner/version"
+
+module Aws
+  module Image
+    module Scanner
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
